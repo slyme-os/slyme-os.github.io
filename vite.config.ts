59631c3c6b34b735eb935ac55ed5a53d1a801186
slyme-os <slyme@tutamail.com>
2025-12-15 02:38:01 +0530

    Comm

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // 'base' is set to './' to support deployment to any path (e.g., user.github.io/repo-name/)
-  base: './',
-})
